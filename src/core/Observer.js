73d45f71e063b33eccff0a10d609e0b7891ad67e
pmzi <pouya.mozafar@gmail.com>
2018-05-23 19:50:57 +0430

    observer now can observe on script(Very basic and limited)

--- src/core/Observer.js
+++ src/core/Observer.js
@@ -3,18 +3,62 @@ const fs = require('fs');
 class Observer{
 
     constructor(files){
+
+        this.files = [];
         
         for(let file of files){
 
-            this.lookForTags(file);
+            if(this.changeFileExistance(file)){
+                this.files.push(file);
+                this.lookForTags(file);
+            }
 
+        }
+
+    }
+
+    observe(file){
+        if(this.changeFileExistance(file)){
+            this.files.push(file);
+            this.lookForTags(file);
         }
 
     }
 
     lookForTags(file){
 
-        const contentOfTheFile = fs.readFileSync(file,"utf8");
+        let contentOfTheFile = fs.readFileSync(file,"utf8");
+
+        let result = /<script(?=.*data-nc).+src=(['"])(.+)\1>.*<\/script>/i.exec(contentOfTheFile);
+
+        let address = result[2];
+
+        fs.watch(address,{encoding:'utf8'},(eventType,fileName)=>{
+            
+            this.changeFileContent(file,result[0],address);
+
+        });
+
+    }
+
+    changeFileExistance(file){
+
+        if(fs.existsSync(file)){
+            return true;
+        }
+        return false;
+
+    }
+
+    changeFileContent(file, tag, address){
+
+        let fileContent = fs.readFileSync(file,"utf8");
+
+        let newTag = tag.replace(address,`${address}?cache=`+Math.random());
+
+        fileContent = fileContent.replace(tag,newTag);
+
+        fs.writeFileSync(file, fileContent);
 
     }
 
